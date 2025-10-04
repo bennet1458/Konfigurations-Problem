9954847ba79d591ed30578cbb3af859d8e57e88d
Bennet Wucherpfennig <bewu@uni-bremen.de>
2025-10-04 13:18:50 +0200

    Slight changes.
    Modified distance_calculation function to put out an upper triangle matrix.
    Modified plot_points function internally to deal with arrays of the shape (1,n).

--- Konfigurations_Problem.py
+++ Konfigurations_Problem.py
@@ -1,4 +1,9 @@
-import numpy as np 
+'''
+This module provides functions to generate random points in 2D space,
+calculate pairwise distances between them, and visualize the points with connecting lines.
+'''
+
+import numpy as np
 import matplotlib.pyplot as plt
 
 def random_points(number_of_points):
@@ -21,13 +26,12 @@ def random_circle_points(number_of_points):
     Returns:
         tuple: Two arrays containing x and y coordinates of the points.
     '''
-    radius = 0.9 + 0.1 * np.random.rand(number_of_points)
+    radius = 0.45 + 0.05 * np.random.rand(number_of_points)
     angles = np.random.rand(number_of_points) * 2 * np.pi
 
     x_positions = radius * np.cos(angles).reshape(1, number_of_points)
     y_positions = radius * np.sin(angles).reshape(1, number_of_points)
     return x_positions, y_positions
-
 
 def distance_calculation(x_positions, y_positions):
     '''
@@ -38,8 +42,11 @@ def distance_calculation(x_positions, y_positions):
     Returns:
         array: 2D array of pairwise distances.
     '''
-    x_distance = x_positions.T - x_positions
-    y_distance = y_positions.T - y_positions
+    x_distance = np.triu(x_positions.T - x_positions)
+    y_distance = np.triu(y_positions.T - y_positions)
+    # x_distance = np.triu(np.tile(x_positions.T, (1 , x_positions.shape[1])), k=1) - np.triu(np.tile(x_positions, (x_positions.shape[1], 1)), k=1)
+    # y_distance = np.triu(np.tile(y_positions.T, (1 , y_positions.shape[1])), k=1) - np.triu(np.tile(y_positions, (y_positions.shape[1], 1)), k=1)
+    
     distance = np.sqrt(x_distance**2 + y_distance**2)
     return distance
 
@@ -53,17 +60,15 @@ def plot_points(x, y):
         None
     '''
     plt.figure()
-    x = x.flatten()
-    y = y.flatten()
-    for i in range(len(x)):
-        for j in range(i+1, len(x)):
-            plt.plot([x[i], x[j]], [y[i], y[j]], color="red")
+    for i in range(x.shape[1]):
+        for j in range(i+1, x.shape[1]):
+            plt.plot([x[0,i], x[0,j]], [y[0,i], y[0,j]], color="red")
     plt.scatter(x, y)
     plt.xlabel("X")
     plt.ylabel("Y")
     plt.show()
     return
 
-points = random_circle_points(10)
+points = random_circle_points(5)
 print(distance_calculation(*points))
 plot_points(*points)
