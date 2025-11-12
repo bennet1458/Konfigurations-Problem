d810bd594b23f1a0404146fa7a52a45504e2ecc5
Bennet Wucherpfennig <bewu@uni-bremen.de>
2025-11-12 13:16:18 +0100

    Changed the program to calculate the distance to the points in set1 from each point in set2. Change the shortest_distance function so the shortest distance from each point in set1 to set2 found. Changed the plot_distance function, so the grid points are used as a heatmap. Canged the circle_points, so the points are spaced equally over the circle and the default argument for the offset is a tuple.

--- Konfigurations_Problem_Teil2.py
+++ Konfigurations_Problem_Teil2.py
@@ -1,28 +1,28 @@
 '''
 Konfigurations_Problem_Teil2.py
-This script generates random points in a circular configuration and grid points in 2D space.
-It calculates pairwise distances between the two sets of points, finds the shortest distance
-from a specific grid point to the other grid and identifies the closest point.
-Finally, it visualizes the distances from a given point in a heatmap.
+This script generates two sets of points in 2D space: one set arranged in a circle
+and another set arranged in a grid. It calculates the pairwise distances between the points 
+from both sets and visualizes the shortest distances from the grid points to the circle points
+as a heatmap. The circle points are plotted on top of the heatmap for reference.
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-def random_circle_points(number_of_points, offset=[0, 0]):
+def circle_points(number_of_points, offset=(0, 0)):
     '''
-    Generate random points in 2D space, in the shape of a circle.
+    Generate points in 2D space, in the shape of a circle.
     Args:
-        number_of_points (int): Number of random points to generate.
+        number_of_points (int): Number of points to generate.
     Returns:
         array: 2D array containing x and y coordinates of the points.
     '''
     radius = 3
-    angles = np.random.rand(number_of_points) * 2 * np.pi
+    angles = np.linspace(2*np.pi/number_of_points, 2*np.pi, number_of_points)
     positions = np.empty((number_of_points, 2))
     positions[:, 0] = radius * np.cos(angles)
     positions[:, 1] = radius * np.sin(angles)
-    positions += offset
+    positions += np.array(offset)
     return positions
 
 def grid_points(nx, ny):
@@ -32,7 +32,7 @@ def grid_points(nx, ny):
         nx (int): Number of points in x direction.
         ny (int): Number of points in y direction.
     Returns:
-        array: 2D array containing x and y coordinates of the points. 
+        array: 2D array containing x and y coordinates of the points.
     '''
     # Meshgrid approach (alternative)
     # xx, yy = np.meshgrid(np.linspace(0, 10, num=nx), np.linspace(0, 10, num=ny))
@@ -56,82 +56,46 @@ def distance_calculation(positions1, positions2):
     distance = np.sqrt(np.sum((positions1[:, None, :] - positions2[None, :, :])**2, axis=2))
     return distance
 
-def grid_point_number(x_number, y_number, nx):
+def shortest_distance(distance):
     '''
-    Calculate the index of a disired point in the positions matrix from its x and y indices.
-    Indices start at 0. Necessary because grid points are stored in an array of dimension (nx*ny,2).
+    Calculate the shortest distance from the points of set2 to any point in set1.
     Args:
-        x_number (int): x index of the point.
-        y_number (int): y index of the point.
-        nx (int): number of points in x direction.
-    Returns:
-        int: index of the desired point.
-    '''
-    return x_number + nx*y_number
-
-def shortest_distance(point_number, distance):
-    '''
-    Calculate the shortest distance from a given point of set2 to all points in set1.
-    Args:
-        point_number (int): index of point in set2 to calculate distances from.
         distance (array): 2D array of pairwise distances between points of set1 and set2.
     Returns:
-        float: shortest distance from the given point to set1.
+        array: 1D array with shortest distance from the points in set2 to any point in set1
     '''
-    return np.min(distance[:, point_number])
+    return np.min(distance, axis=0)
 
-def closest_point(point_number, distance):
+def plot_distance(positions1, positions2, shortest_dist, nx, ny):
     '''
-    Calculate the index of the point in set1 that is closest to a given point in set2.
-    Args:
-        point_number (int): point number in set2 to calculate distances from.
-        distance (array): 2D array of pairwise distances between points of set1 and set2.
-    Returns:
-        int: index of point in set1 that is closest to the given point in set2.
-    '''
-    return np.argmin(distance[:, point_number])
-
-def plot_distance(positions1, positions2, centerpoint, close_point):
-    '''
-    Plot the distances from a given point in set2 in a heatmap.
-    Plot also shows the points of both sets, highlighting the given point in set2 and the closest point in set1.
+    Plot the grid set2 as a heatmap with the color corresponding to the distance to set1. 
+    Also plot the points of set1 on top of the heatmap. 
     Args:
         positions1 (array): 2D array of x and y coordinates of set1.
         positions2 (array): 2D array of x and y coordinates of set2.
-        centerpoint (array): x and y coordinates of the point in set2 to calculate distances from.
-        close_point (int): index of the point in set1 that is closest to the given point in set2.
+        shortest_dist (array): 1D array with shortest distances from set2 to set1.
+        nx (int): Number of points in x direction for set2.
+        ny (int): Number of points in y direction for set2.
     Returns:
         None
     '''
-    x_min = min(0, np.min(positions1[:, 0]))-1
-    x_max = max(10, np.max(positions1[:, 0]))+1
-    y_min = min(0, np.min(positions1[:, 1]))-1
-    y_max = max(10, np.max(positions1[:, 1]))+1
-
-    x = np.linspace(x_min, x_max, num=100)
-    y = np.linspace(y_min, y_max, num=100)
-    z = np.sqrt((x-centerpoint[0])[None, :]**2 + (y-centerpoint[1])[:, None]**2)
-
-    plt.imshow(z, extent=[x_min, x_max, y_min, y_max], origin='lower')
-
-    plt.scatter(positions2[:, 0], positions2[:, 1], color='green')
-    plt.scatter(centerpoint[0], centerpoint[1], color='red')
-    plt.scatter(positions1[:, 0], positions1[:, 1], color='blue')
-    plt.scatter(positions1[close_point, 0], positions1[close_point, 1], color='red')
-
+    grid = np.reshape(shortest_dist, shape=(ny, nx))
+    x_min = np.min(positions2[:, 0])
+    x_max = np.max(positions2[:, 0])
+    y_min = np.min(positions2[:, 1])
+    y_max = np.max(positions2[:, 1])
+    plt.imshow(grid, extent=[x_min, x_max, y_min, y_max], origin='lower')
+    plt.colorbar()
+    plt.scatter(positions1[:, 0], positions1[:, 1], color='red')
     plt.show()
     return
 
 
-n_circ = 10
-n_x = 10
-n_y = 3
-points1 = random_circle_points(n_circ, offset=[5, 14])
+n_circ = 5
+n_x = 100
+n_y = 40
+points1 = circle_points(n_circ, offset=(5, 5))
 points2 = grid_points(n_x, n_y)
-
 dist = distance_calculation(points1, points2)
-point_numb = grid_point_number(7, 1, n_x)
-print(shortest_distance(point_numb, dist))
-c_point = closest_point(point_numb, dist)
 
-plot_distance(points1, points2, points2[point_numb], c_point)
+plot_distance(points1, points2, shortest_distance(dist), n_x, n_y)
